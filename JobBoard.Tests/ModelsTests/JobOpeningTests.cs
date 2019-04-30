0ee27bda5e304c81711b31d818615cb89b25aae5
Brian Hensley and Jared Farkas <brian@>
2019-04-30 15:05:17 -0700

    convert Getters & Setters to shorthand

--- JobBoard.Tests/ModelsTests/JobOpeningTests.cs
+++ JobBoard.Tests/ModelsTests/JobOpeningTests.cs
@@ -21,45 +21,45 @@ namespace JobBoard.Tests
       Assert.AreEqual(typeof(JobOpening), newJobOpening.GetType());
     }
 
-    [TestMethod]
-    public void GetTitle_ReturnsTitle_True()
-    {
-      JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
-      string newTitle = newJobOpening.GetTitle();
-      Assert.AreEqual("test1", newTitle);
-    }
-
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_True()
-    {
-      JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
-      string newDescription = newJobOpening.GetDescription();
-      Assert.AreEqual("test2", newDescription);
-    }
-
-    [TestMethod]
-    public void GetName_ReturnsName_True()
-    {
-      JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
-      string newName = newJobOpening.GetName();
-      Assert.AreEqual("test3", newName);
-    }
-
-    [TestMethod]
-    public void GetAddress_ReturnsAddress_True()
-    {
-      JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
-      string newAddress = newJobOpening.GetAddress();
-      Assert.AreEqual("test4", newAddress);
-    }
-
-    [TestMethod]
-    public void GetPhone_ReturnsPhone_True()
-    {
-      JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
-      string newPhone = newJobOpening.GetPhone();
-      Assert.AreEqual("test5", newPhone);
-    }
+    // [TestMethod]
+    // public void GetTitle_ReturnsTitle_True()
+    // {
+    //   JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
+    //   string newTitle = newJobOpening.GetTitle();
+    //   Assert.AreEqual("test1", newTitle);
+    // }
+    //
+    // [TestMethod]
+    // public void GetDescription_ReturnsDescription_True()
+    // {
+    //   JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
+    //   string newDescription = newJobOpening.GetDescription();
+    //   Assert.AreEqual("test2", newDescription);
+    // }
+    //
+    // [TestMethod]
+    // public void GetName_ReturnsName_True()
+    // {
+    //   JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
+    //   string newName = newJobOpening.GetName();
+    //   Assert.AreEqual("test3", newName);
+    // }
+    //
+    // [TestMethod]
+    // public void GetAddress_ReturnsAddress_True()
+    // {
+    //   JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
+    //   string newAddress = newJobOpening.GetAddress();
+    //   Assert.AreEqual("test4", newAddress);
+    // }
+    //
+    // [TestMethod]
+    // public void GetPhone_ReturnsPhone_True()
+    // {
+    //   JobOpening newJobOpening = new JobOpening("test1", "test2", "test3", "test4", "test5");
+    //   string newPhone = newJobOpening.GetPhone();
+    //   Assert.AreEqual("test5", newPhone);
+    // }
 
   }
 }
--- JobBoard/Models/JobOpening.cs
+++ JobBoard/Models/JobOpening.cs
@@ -12,6 +12,14 @@ namespace JobBoard.Models
     private string _phone;
     private static List<JobOpening> _openings = new List<JobOpening>{};
 
+
+    //Getters & Setters call with myJobOpening.title to Get myJobOpening.title = "whatever" to Set
+    public string title { get => _title; set => _title = value;}
+    public string description { get => _description; set => _description = value;}
+    public string name { get => _name; set => _name = value;}
+    public string address { get => _address; set => _address = value;}
+    public string phone { get => _phone; set => _phone = value;}
+
     public JobOpening(string title, string description, string name, string address, string phone)
     {
       _title = title;
@@ -22,62 +30,84 @@ namespace JobBoard.Models
       _openings.Add(this);
     }
 
-    //Getters
-    public string GetTitle()
-    {
-      return _title;
-    }
-
-    public string GetDescription()
-    {
-      return _description;
-    }
-
-    public string GetName()
-    {
-      return _name;
-    }
-
-    public string GetAddress()
-    {
-      return _address;
-    }
-
-    public string GetPhone()
-    {
-      return _phone;
-    }
-
     public static List<JobOpening> GetAll()
     {
       return _openings;
     }
-
-    public void SetTitle(string newTitle)
-    {
-      _title = newTitle;
-    }
-
-    public void SetDescription(string newDescription)
-    {
-      _description = newDescription;
-    }
-
-    public void SetName(string newName)
-    {
-      _name = newName;
-    }
-
-    public void SetAddress(string newAddress)
-    {
-      _address = newAddress;
-    }
-
-    public void SetPhone(string newPhone)
-    {
-      _phone = newPhone;
-    }
-
-
   }
 }
+
+// Getters
+// public string GetTitle()
+// {
+//   return _title;
+// }
+//
+// public string GetDescription()
+// {
+//   return _description;
+// }
+//
+// public string GetName()
+// {
+//   return _name;
+// }
+//
+// public string GetAddress()
+// {
+//   return _address;
+// }
+//
+// public string GetPhone()
+// {
+//   return _phone;
+// }
+//
+//
+// public void SetTitle(string newTitle)
+// {
+//   _title = newTitle;
+// }
+//
+// public void SetDescription(string newDescription)
+// {
+//   _description = newDescription;
+// }
+//
+// public void SetName(string newName)
+// {
+//   _name = newName;
+// }
+//
+// public void SetAddress(string newAddress)
+// {
+//   _address = newAddress;
+// }
+//
+// public void SetPhone(string newPhone)
+// {
+//   _phone = newPhone;
+// }
+//
+//
+// }
+// }
+//
+//
+// this is an easy way to do get/set with a bunch of string properties
+//
+// private string[] contact = new string[5];
+//
+// public JobOpening(string title, string description, string name, string address, string phone)
+// {
+//   contact[0] = title;
+//   contact[1] = description;
+//   contact[2] = name;
+//   contact[3] = address;
+//   contact[4] = phone;
+//   _openings.Add(this);
+// }
+// public string GetWhatever(int ind)
+// {
+//   return contact[ind];
+// }
