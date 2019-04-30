6950342dc8a526c0691849009c0d22a1a1d152d3
Brian Hensley and Jared Farkas <jared@>
2019-04-30 11:52:15 -0700

    build file structure

---
+++ JobBoard/Models/Application.cs
@@ -0,0 +1,1 @@
+// Do this is finished early
---
+++ JobBoard/Models/JobOpening.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace JobBoard.Models
+{
+  public class JobOpening
+  {
+    private string _title;
+    private string _description;
+    private string _name;
+    private string _address;
+    private string _phone;
+    private static List<JobOpening> _openings = new List<JobOpening>
+  }
+}
