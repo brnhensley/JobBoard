9dc2163872fb5dd42af05f15369e7a206d6b31b1
Brian Hensley and Jared Farkas <jared@>
2019-04-30 12:00:12 -0700

    add Getters and object constructor

--- JobBoard/Models/JobOpening.cs
+++ JobBoard/Models/JobOpening.cs
@@ -10,6 +10,49 @@ namespace JobBoard.Models
     private string _name;
     private string _address;
     private string _phone;
-    private static List<JobOpening> _openings = new List<JobOpening>
+    private static List<JobOpening> _openings = new List<JobOpening>{};
+
+    public JobOpening(string title, string description, string name, string address, string phone)
+    {
+      _title = title;
+      _description = description;
+      _name = name;
+      _address = address;
+      _phone = phone;
+      _openings.Add(this);
+    }
+
+    //Getters
+    public string GetTitle()
+    {
+      return _title;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public string GetName()
+    {
+      return _address;
+    }
+
+    public string GetAddress()
+    {
+      return _address;
+    }
+
+    public string GetPhone()
+    {
+      return _phone;
+    }
+
+    public static List<JobOpening> GetAll()
+    {
+      return _openings;
+    }
+
+
   }
 }
